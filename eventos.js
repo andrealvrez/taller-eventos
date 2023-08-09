0b39d46c96982fa0092cf7dade259d928abf5e32
Andrea Alvarez <andreavaleria.951@gmail.com>
2023-08-08 23:18:58 -0300

    alerta del div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+   
+    const boton= document.getElementById('boton');
+
+    boton.addEventListener("click", function(){
+        alert("Hola! Soy el div");
+    });
+
+});
